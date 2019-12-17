99b1886772d5c6e7d93ba59cf670988ad89db3bb
Valenca, Priscylla D <priscylla.d.valenca@accenture.com>
2019-12-17 16:45:13 -0300

    ShadPreferences creation

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.weatherapp
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+  private val sp: SharedPreferences by lazy {
+    getSharedPreferences("my_sp", Context.MODE_PRIVATE)
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+
+  }
+
+  override fun onResume() {
+    super.onResume()
+    readRadioButton()
   }
 
 
@@ -31,6 +45,18 @@ class MainActivity : AppCompatActivity() {
       }
       else -> return super.onOptionsItemSelected(item)
     }
+  }
+
+
+  private fun readRadioButton() {
+
+    sp.getString("TemperatureUnit", "°C")
+    sp.getString("TemperatureUnit", "ºF")
+    sp.getString("TemperatureLanguage", "EN")
+    sp.getString("TemperatureLanguage", "PE")
+
+    textView.text = "Temperatura selecionada:\n\n ${sp.getString("TemperatureUnit", "°C")}\n\n Linguagem selecionada:\n\n ${sp.getString("TemperatureLanguage", "PT")} "
+
   }
 
 }
--- app/src/main/java/com/example/weatherapp/Settings.kt
+++ app/src/main/java/com/example/weatherapp/Settings.kt
@@ -1,18 +1,78 @@
 package com.example.weatherapp
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.RadioButton
+
+const val TAG = "MainActivity"
+const val FILE_NAME = "My_file"
 
 class Settings : AppCompatActivity() {
+
+  private val sp: SharedPreferences by lazy {
+    getSharedPreferences("my_sp", Context.MODE_PRIVATE)
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_settings)
 
   }
+
+
+  fun onRadioButtonClickedUnit(view: View) {
+    if (view is RadioButton) {
+      // Is the button now checked?
+      val checked = view.isChecked
+
+      // Check which radio button was clicked
+      when (view.getId()) {
+        R.id.radioButtonC ->
+          if (checked) {
+            sp.edit()
+              .putString("TemperatureUnit", "°C")
+              .apply()
+          }
+        R.id.radioButtonF ->
+          if (checked) {
+            sp.edit()
+              .putString("TemperatureUnit", "ºF")
+              .apply()
+          }
+      }
+    }
+  }
+
+
+  fun onRadioButtonClickedLanguage(view: View) {
+    if (view is RadioButton) {
+      // Is the button now checked?
+      val checked = view.isChecked
+
+      // Check which radio button was clicked
+      when (view.getId()) {
+        R.id.radioButtonEN ->
+          if (checked) {
+            sp.edit()
+              .putString("TemperatureLanguage", "EN")
+              .apply()
+          }
+        R.id.radioButtonPT ->
+          if (checked) {
+            sp.edit()
+              .putString("TemperatureLanguage", "PT")
+              .apply()
+          }
+      }
+    }
+  }
+
 
 
 
